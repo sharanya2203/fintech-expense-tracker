218499c5a66bb1633328ade72115a8a2e9e47d74
sharanya2203 <sharanyaponu6@gmail.com>
2026-06-08 21:22:19 +0530

    Fix database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,11 @@
 const { Pool } = require("pg");
-require("dotenv").config();
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
-
-pool.connect()
-  .then(() => {
-    console.log("✅ PostgreSQL Connected");
-  })
-  .catch((err) => {
-    console.error("❌ PostgreSQL Error:");
-    console.error(err);
-  });
 
 module.exports = pool;
